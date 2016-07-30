4315b246d9cd7121d06e79e4ac848c6e25dfc079
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-29 23:44:31 -0500

    comments and statements

---
+++ app.js
@@ -0,0 +1,5 @@
+/****************************************************
+*               Comments and Statements             *
+****************************************************/
+document.write("I love ham");
+document.write("and I love and bacon too");
